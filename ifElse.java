2d3954d983505a8af6d3f9b91e3379544c5eab54
Partha Majumder <parthamajumder9810@gmail.com>
2024-06-15 22:32:16 -0400

    java-prgoram0.2

--- ifElse.java
+++ ifElse.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class ifElse {
     public static void main (String[] args){
-        System.out.println("Good morning");
+        int user;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the all number");
+        user = scanner.nextInt();
+
+        if (user % 2 ==0)
+        {
+            System.out.println("Enter the Even number ");
+        }else {
+            System.out.println("Enter the odd number ");
+        }
+
     }
 }
